b0213777e4fe1f812470e90b2827fdee0af428e7
luhrocha <luanarocha17@gmail.com>
2023-01-16 23:31:31 -0300

    first part

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+import {PrismaClient} from '@prisma/client'
+import cors from '@fastify/cors'
+
+const app = fastify();
+const prisma = new PrismaClient()
+app.register(cors)
+
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany()
+    return habits
+})
+
+app.listen({port: 3333}).then(() => {
+    console.log('HTTP Server Running')
+})
